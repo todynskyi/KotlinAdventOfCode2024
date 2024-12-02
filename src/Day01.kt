d218fb41a21187e1c791c28276830e44318f4fe6
todynskyi <todynskyi@gmail.com>
2024-12-02 01:05:07 +0100

    Added Advent of Code Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,31 @@
+import java.util.PriorityQueue
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val pairs = toPairs(input)
+        val leftList = PriorityQueue(pairs.map { it.first })
+        val rightList = PriorityQueue(pairs.map { it.second })
+
+        var sum = 0
+        while (leftList.isNotEmpty()) {
+            sum += abs(rightList.poll() - leftList.poll())
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val pairs = toPairs(input)
+        val counts = pairs.map { it.second }.groupingBy { it }.eachCount()
+
+        return pairs.sumOf { it.first * counts.getOrDefault(it.first, 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun toPairs(input: List<String>, delimiter: String = "   "): List<Pair<Int, Int>> =
+    input.map { it.split(delimiter) }
+        .map { it[0].toInt() to it[1].toInt() }
